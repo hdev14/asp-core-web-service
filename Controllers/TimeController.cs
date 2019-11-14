c02e515d073247d0911247437bfbd763d8e785cc
HermersonDev <ne.hermerson@gmail.com>
2019-11-14 19:34:58 -0300

    Imp. do TimeRepository e TimeController.

---
+++ Controllers/TimeController.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using web_service.Models;
+using web_service.Repositories;
+
+namespace web_service.Controllers
+{
+    public class TimeController : ControllerBase
+    {
+        private readonly TimeRepository repository
+        public TimeController(TimeRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Time>> Get(int id)
+        {
+            var time = await repository.FindTimeAsync(id);
+            if (time != null)
+                return time;
+
+            return NotFound();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Time>>> Get()
+        {
+            var times = await repository.GetTimesAsync();
+
+            if (times != null)
+                return times;
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Time time)
+        {
+            if (await repository.CreateTimeAsync(time))
+                return RedirectToAction("Get", new { id = time.Id });
+
+            return StatusCode(500, new
+            {
+                message = "Não foi possível registrar o time."
+            });
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, Time time)
+        {
+            if (await repository.UpdateTimeAsync(id, time))
+                return RedirectToAction("Get", new { id = id });
+
+            return StatusCode(500, new {
+                message = "Não foi possível atualizar o time."
+            });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            if (await repository.DeleteTimeAsync(id))
+            {
+                return Ok(new {
+                    message = "Time excluído com sucesso !"
+                });
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ Repositories/TimeRepository.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using web_service.database;
+using web_service.Models;
+
+namespace web_service.Repositories
+{
+    public class TimeRepository
+    {
+        private readonly WebServiceContext context;
+
+        public TimeRepository(WebServiceContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<bool> CreateTimeAsync(Time time)
+        {
+            context.Times.Add(time);
+            return (await context.SaveChangesAsync() != 0);
+        }
+
+        public async Task<bool> UpdateTimeAsync(int id, Time t)
+        {
+            var time = await this.FindTimeAsync(id);
+            if (time != null)
+            {
+                time.Nome = t.Nome;
+                return (await context.SaveChangesAsync() != 0);
+            }
+
+            return false;
+        }
+
+        public async Task<bool> DeleteTimeAsync(int id)
+        {
+            var time = await this.FindTimeAsync(id);
+            if (time != null)
+            {
+                context.Times.Remove(time);
+                return (await context.SaveChangesAsync() != 0);
+            }
+
+            return false;
+        }
+
+        public async Task<Time> FindTimeAsync(int id)
+        {
+            return await context.Times.FindAsync(id);
+        }
+
+        public async Task<List<Time>> GetTimesAsync()
+        {
+            return await context.Times.ToListAsync();
+        }
+    }
+}
