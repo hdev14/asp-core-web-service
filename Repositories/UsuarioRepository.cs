90cf69115ca4df88ce511faa86f99150da1675bb
HermersonDev <ne.hermerson@gmail.com>
2019-11-19 19:36:48 -0300

    Adiçao de metodo para recupera o usuario pelo nome.

--- Repositories/UsuarioRepository.cs
+++ Repositories/UsuarioRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using web_service.database;
@@ -13,6 +14,16 @@ namespace web_service.Repositories
         public UsuarioRepository(WebServiceContext context)
         {
             this.context = context;
+        }
+
+        public async Task<Usuario> FindUsuarioAsync(int id)
+        {
+            return await context.Usuarios.FindAsync(id);
+        }
+
+        public async Task<List<Usuario>> GetUsuariosAsync()
+        {
+            return await context.Usuarios.ToListAsync();
         }
 
         public async Task CreateUsuarioAsync(Usuario usuario)
@@ -51,15 +62,9 @@ namespace web_service.Repositories
             return false;
         }
 
-        public async Task<Usuario> FindUsuarioAsync(int id)
+        public async Task<Usuario> GetUsuarioByUsername(string username)
         {
-            return await context.Usuarios.FindAsync(id);
+            return await context.Usuarios.Where(u => u.Username == username).FirstOrDefaultAsync();
         }
-
-        public async Task<List<Usuario>> GetUsuariosAsync()
-        {
-            return await context.Usuarios.ToListAsync();
-        }
-
     }
 }
