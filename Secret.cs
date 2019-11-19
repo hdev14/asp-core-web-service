4c1add12f6892b80d68e0c3291fe77292f9cd5a3
HermersonDev <ne.hermerson@gmail.com>
2019-11-19 14:12:03 -0300

    Imp. da class Secret e inicio da criaçao do serviço para geraçao de jwttoken.

---
+++ Secret.cs
@@ -0,0 +1,7 @@
+namespace web_service
+{
+    public static class Secret
+    {
+        public static string hash = "6ffecf34f117ab67aa8e1a2ff49e5dff";
+    }
+}
---
+++ Services/Auth/TokenJwt.cs
@@ -0,0 +1,12 @@
+using web_service.Models;
+
+namespace web_service.Services.Auth
+{
+    public static class TokenJwt
+    {
+        public static string GenerateToken(Usuario usuario)
+        {
+            return "";
+        }
+    }
+}
