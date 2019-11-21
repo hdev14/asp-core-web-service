46f9e735604e3134e62ad2a5f637b9245a735c84
HermersonDev <ne.hermerson@gmail.com>
2019-11-21 19:13:11 -0300

    Algumas modificaçoes.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 using System.Threading.Tasks;
-using CryptSharp;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using web_service.Models;
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using CryptSharp;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using web_service.Models;
